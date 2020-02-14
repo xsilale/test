64751bd521a87a0a6dc5c749f4e325f49e4627a1
Alexander Silkin <xsilale@gmail.com>
2020-02-14 20:18:46 +0300

    Second part: TCP connection logic and Chat Server logic added

--- server/src/chat/server/ChatServer.java
+++ server/src/chat/server/ChatServer.java
@@ -1,7 +1,49 @@
 package chat.server;
 
-public class ChatServer {
+import chat.network.TCPConnection;
+import chat.network.TCPConnectionListener;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class ChatServer implements TCPConnectionListener {
     public static void main(String[] args) {
+        new ChatServer();
+
+    }
+
+    private ChatServer(){
+        System.out.println("Server running... ");
+        try(ServerSocket serverSocket = new ServerSocket(8189)){
+            while(true) {
+                try {
+                    new TCPConnection(this, serverSocket.accept());
+                }
+            }catch (IOException e) {
+                    System.out.println("TCPConnection exception: " + e);
+                }
+        }catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public synchronized void onConnectionReady(TCPConnection tcpConnection) {
+
+    }
+
+    @Override
+    public synchronized void onReceiveString(TCPConnection tcpConnection, String value) {
+
+    }
+
+    @Override
+    public synchronized void OnDisconnect(TCPConnection tcpConnection) {
+
+    }
+
+    @Override
+    public synchronized void onException(TCPConnection tcpConnection, Exception e) {
 
     }
 }
