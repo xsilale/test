6690c0abfd5e32021db18ebf2626bb374a595bd9
Alexander Silkin <xsilale@gmail.com>
2020-02-18 00:50:33 +0300

    Client has been done

--- client/src/chat/client/ClientWindow.java
+++ client/src/chat/client/ClientWindow.java
@@ -1,7 +1,103 @@
 package chat.client;
 
-public class ClientWindow {
-    public static void main(String[] args) {
+import chat.network.TCPConnection;
+import chat.network.TCPConnectionListener;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.sql.Connection;
+
+public class ClientWindow extends JFrame implements ActionListener, TCPConnectionListener {
+
+    private static final String IP_ADDR = "192.168.220.53";
+    private static final int PORT = 8189;
+    private static final int WIDTH = 600;
+    private static final int HEIGHT = 400;
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ClientWindow();
+            }
+        });
+
+
+
+
+    }
+
+    private final JTextArea log = new JTextArea();
+    private final JTextField fieldNickname = new JTextField("xsilale");
+    private final JTextField fieldInput = new JTextField();
+
+    private TCPConnection connection;
+
+    private ClientWindow(){
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(WIDTH, HEIGHT);
+        setLocationRelativeTo(null);
+        setAlwaysOnTop(true);
+        setVisible(true);
+
+        log.setEditable(false);
+        log.setLineWrap(true);
+        add(log, BorderLayout.CENTER);
+
+        fieldInput.addActionListener(this);
+        add(fieldNickname, BorderLayout.NORTH);
+        add(fieldInput, BorderLayout.SOUTH);
+        try {
+            connection = new TCPConnection(this, IP_ADDR,PORT);
+        } catch (IOException e) {
+            printMsg("Connection exception... " + e);
+        }
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        String msg = fieldInput.getText();
+        if (msg.equals("")) return;
+        fieldInput.setText(null);
+        connection.sendString(fieldNickname.getText() + ": " + msg);
+
+    }
+
+    @Override
+    public void onConnectionReady(TCPConnection tcpConnection) {
+        printMsg("Connection ready");
+
+    }
+
+    @Override
+    public void onReceiveString(TCPConnection tcpConnection, String value) {
+        printMsg(value);
+
+    }
+
+    @Override
+    public void OnDisconnect(TCPConnection tcpConnection) {
+        printMsg("Connection closed");
+
+    }
+
+    @Override
+    public void onException(TCPConnection tcpConnection, Exception e) {
+        printMsg("Connection exception... " + e);
+
+    }
+
+    private synchronized void printMsg (String msg){
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                log.append(msg + "\n");
+                log.setCaretPosition(log.getDocument().getLength());
+            }
+        });
     }
 }
